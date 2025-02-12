f4ab67b325b638b8388091877ad6a11514937881
NeiJunio <neijunio50@gmail.com>
2025-02-12 02:41:02 -0300

    feat: Correção da estrutura e lógica, implementação lógica de cadastro e estilização da página

--- TesteWebSINQIA/Controllers/ListaPontosTuristicosController.cs
+++ TesteWebSINQIA/Controllers/ListaPontosTuristicosController.cs
@@ -18,5 +18,25 @@ namespace TesteWebSINQIA.Controllers
             IEnumerable<PontosTuristicosModel> pontosTuristicos = _dbContext.PontosTuristicos; // busca a model, depois define o nome "pontosTuristicos", em seguida acessa o banco, por meio da váriavel _dbContext, e logo após, acessa a tabela PontosTuristicos por meio do sufixo ".PontosTuristicos"
             return View(pontosTuristicos); // retorna o Inumerable pontosTuristicos dentor da view
         }
+
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(PontosTuristicosModel pontosTuristicos)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.PontosTuristicos.Add(pontosTuristicos); // Envia os dados para o banco
+                _dbContext.SaveChanges(); // Salva as alterações no banco
+
+                return RedirectToAction(nameof(Index)); // Sai da página de cadastro e automaticamente redireciona para a página index
+            }
+
+            return View(pontosTuristicos);
+        }
     }
 }
+
